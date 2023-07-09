c6a939f9e43aa2addd69aaacc5d31d329b51aad8
27tonorth <27tonorth@gmail.com>
2023-07-09 22:31:11 +0700

    change card

--- src/Components/FeatureCard.js
+++ src/Components/FeatureCard.js
@@ -1,17 +1,16 @@
 import React from 'react';
 
 function FeatureCard({ dialogue, image, name, position}) {
+  const isRightAligned = name === 'รองศาสตราจารย์ ดร. วาสนา วงศ์สุรวัฒน์'; 
 
   return (
-    <div className="feature-card">
-      <div className="feature-card__content">
-        <p className="feature-card__dialogue">{dialogue}</p>
-      </div>
+    <div className={`feature-card ${isRightAligned ? 'right-aligned' : ''}`}>
+      <div className={`feature-card__dialogue ${isRightAligned ? 'text-align-end' : ''}`}>{dialogue}</div>
       <div className="feature-card__profile">
-        <div className="feature-card__image-container">
+        <div className={`feature-card__image-container ${isRightAligned ? 'text-align-end' : ''}`}>
           <img src={image} alt="" className="feature-card__image" />
         </div>
-        <div className="feature-card__profile-content">
+        <div className={`feature-card__profile-content ${isRightAligned ? 'text-align-end' : ''}`}>
           <h3 className="feature-card__name">{name}</h3>
           <p className="feature-card__position">{position}</p>
         </div>
